11d2a5e103cc36108a16e79d7815e3941b9f7eee
Alexandru Carbunaru <alex.carbunaru@gmail.com>
2025-07-18 16:17:07 +0300

    Added update features for current Task fields

--- FamilyTaskManagerAPI/Controllers/TaskItemsController.cs
+++ FamilyTaskManagerAPI/Controllers/TaskItemsController.cs
@@ -4,6 +4,7 @@ using FamilyTaskManagerAPI.Services;
 using FamilyTaskManagerAPI.Utils;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace FamilyTaskManagerAPI.Controllers
 {
@@ -29,12 +30,6 @@ namespace FamilyTaskManagerAPI.Controllers
                 return BadRequest(ModelState);
             }
 
-            string? userId = _userHandler.GetUserId();
-            if (string.IsNullOrEmpty(userId))
-            {
-                return Unauthorized("User is not authenticated.");
-            }
-
             TaskItem input = dto.ToTaskItem();
             try
             {
@@ -52,6 +47,124 @@ namespace FamilyTaskManagerAPI.Controllers
                     title: "An error occurred while creating the task item.");
             }
             return Ok("Task item created successfully.");
+        }
+
+        [HttpPut("{taskId}/assignUser/{userId}")]
+        public async Task<IActionResult> AssignUserToTaskItem(int taskId, string userId)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _taskItemService.AssignUserToTaskItemAsync(taskId, userId);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(
+                    detail: ex.Message,
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    title: "An error occurred while assigning the user to the task item.");
+            }
+            return Ok("User assigned to task item successfully.");
+        }
+
+        [HttpPut("{taskId}/updateStatus/{newStatus}")]
+        public async Task<IActionResult> UpdateTaskItemStatus(int taskId, string newStatus)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                await _taskItemService.UpdateTaskStatusAsync(taskId, newStatus);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(
+                    detail: ex.Message,
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    title: "An error occurred while updating the task item status.");
+            }
+            return Ok("Task item status updated successfully.");
+        }
+
+        [HttpPut("{taskId}/updateDueDate")]
+        public async Task<IActionResult> UpdateTaskItemDueDate(int taskId, [FromBody] DateTime dueDate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _taskItemService.UpdateTaskDueDateAsync(taskId, dueDate);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(
+                    detail: ex.Message,
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    title: "An error occurred while updating the task item due date.");
+            }
+            return Ok("Task item due date updated successfully.");
+        }
+
+        [HttpPut("{taskId}/updateDescription")]
+        public async Task<IActionResult> UpdateTaskItemDescription(int taskId, [FromBody] string description)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            try
+            {
+                await _taskItemService.UpdateTaskItemDescriptionAsync(taskId, description);
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (ArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                return Problem(
+                    detail: ex.Message,
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    title: "An error occurred while updating the task item description.");
+            }
+            return Ok("Task item description updated successfully.");
         }
     }
 }
--- FamilyTaskManagerAPI/Services/TaskItemService.cs
+++ FamilyTaskManagerAPI/Services/TaskItemService.cs
@@ -15,29 +15,116 @@ namespace FamilyTaskManagerAPI.Services
 
         public async Task<int> CreateTaskItemAsync(TaskItem taskItem)
         {
-            // Check if a task with the same title already exists
-            var existingTask = await _context.Tasks.FirstOrDefaultAsync(t => t.Title == taskItem.Title);
-            if (existingTask != null)
-            {
-                throw new ArgumentException("A task with the same title already exists.");
-            }
+            // Check if another task with the same title exists
+            await ValidateTitle(taskItem);
 
-            // Check if task has an assigned user
-            if (taskItem.AssignedUserId != null)
-            {
-                var assignedUser = await _context.Users.FindAsync(taskItem.AssignedUserId);
-                if (assignedUser == null)
-                {
-                    throw new ArgumentException("Assigned user does not exist.");
-                }
-                taskItem.AssignedUserId = assignedUser.Id;
-            }
+            // Check if the user exists if provided
+            await ValidateAssignedUserExistsAsync(taskItem.AssignedUserId);
 
             // Add to context and save changes
             _context.Tasks.Add(taskItem);
             await _context.SaveChangesAsync();
             return taskItem.Id;
         }
+
+        public async Task AssignUserToTaskItemAsync(int taskId, string userId)
+        {
+            // Find the task item
+            var taskItem = await GetTaskItemByIdAsync(taskId);
+
+            // Check if the user exists
+            await ValidateAssignedUserExistsAsync(userId);
+
+            // Assign the user to the task item
+            taskItem.AssignedUserId = userId;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTaskStatusAsync(int taskId, string newStatus)
+        {
+            // Validate the new status
+            var status = await ValidateStatus(newStatus);
+
+            // Find the task item
+            var taskItem = await GetTaskItemByIdAsync(taskId);
+
+            // Update the status
+            taskItem.Status = status;
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task UpdateTaskDueDateAsync(int taskId, DateTime dueDate)
+        {
+            // Find the task item
+            var taskItem = await GetTaskItemByIdAsync(taskId);
+
+            // Validate the due date
+            ValidateDueDate(dueDate);
+
+            // Update the due date
+            taskItem.DueDate = dueDate;
+            await _context.SaveChangesAsync();
+        }
+
+        internal async Task UpdateTaskItemDescriptionAsync(int taskId, string description)
+        {
+            // Find the task item
+            var taskItem = await GetTaskItemByIdAsync(taskId);
+
+            // Update the description
+            taskItem.Description = description;
+            await _context.SaveChangesAsync();
+        }
+
+        private async Task<TaskItemStatus> ValidateStatus(string status)
+        {
+            if (!Enum.TryParse<TaskItemStatus>(status, out var taskStatus))
+            {
+                throw new ArgumentException("Invalid status value.");
+            }
+            return taskStatus;
+        }
+
+        private async Task ValidateTitle(TaskItem taskItem)
+        {
+            // Check if a task with the same title already exists
+            var existingTask = await _context.Tasks.FirstOrDefaultAsync(t => t.Title == taskItem.Title);
+            if (existingTask != null)
+            {
+                throw new ArgumentException("A task with the same title already exists.");
+            }
+        }
+
+        private void ValidateDueDate(DateTime dueDate)
+        {
+            if (dueDate < DateTime.UtcNow)
+            {
+                throw new ArgumentException("Due date cannot be in the past.");
+            }
+        }
+
+        private async Task<TaskItem> GetTaskItemByIdAsync(int taskId)
+        {
+            var taskItem = await _context.Tasks.FindAsync(taskId);
+            if (taskItem == null)
+            {
+                throw new KeyNotFoundException("Task item does not exist.");
+            }
+            return taskItem;
+        }
+
+        private async Task ValidateAssignedUserExistsAsync(string? assignedUserId)
+        {
+            if (assignedUserId != null)
+            {
+                var user = await _context.Users.FindAsync(assignedUserId);
+                if (user == null)
+                {
+                    throw new ArgumentException("Assigned user does not exist.");
+                }
+            }
+        }
+
 
     }
 }
