a621f02f6a7f417d1e20339b058fcdd58c2aa89e
Alexandru Carbunaru <alex.carbunaru@gmail.com>
2025-09-16 11:12:39 +0300

    refactor program.cs to have a better structure and implement ErrorResponseDTO

---
+++ FamilyTaskManagerAPI/DTOs/Responses/ErrorResponseDTO.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace FamilyTaskManagerAPI.DTOs.Responses
+{
+    public class ErrorResponseDTO
+    {
+        public int StatusCode { get; set; }
+        public string Error { get; set; } = string.Empty;
+
+        public DateTime Timestamp { get; set; }
+        public string? Path { get; set; }
+        public List<string>? ValidationErrors { get; set; }
+
+    }
+}
--- FamilyTaskManagerAPI/Program.cs
+++ FamilyTaskManagerAPI/Program.cs
@@ -1,5 +1,6 @@
 using FamilyTaskManagerAPI.Data;
 using FamilyTaskManagerAPI.Data.Repositories;
+using FamilyTaskManagerAPI.DTOs.Responses;
 using FamilyTaskManagerAPI.Entities;
 using FamilyTaskManagerAPI.Middlewares;
 using FamilyTaskManagerAPI.Services;
@@ -11,14 +12,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Net;
 using System.Text;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+#region Database
+
 // Add DbContext for Entity Framework Core
-// Use PostgreSQL
 builder.Services.AddDbContext<TaskManagerDbContext>(options =>
+    // Use PostgreSQL
     options.UseNpgsql(
         builder.Configuration.GetConnectionString("PostgreSQL_Connection"),
         npgsqlOptions => npgsqlOptions.EnableRetryOnFailure(
@@ -28,8 +33,9 @@ builder.Services.AddDbContext<TaskManagerDbContext>(options =>
         )
     );
 
+#endregion
 
-    
+#region Authentication and Authorization
 
 // Configure JWT authentication
 builder.Services.AddAuthentication(options =>
@@ -54,10 +60,55 @@ builder.Services.AddAuthentication(options =>
 // Init JwtSettings from environment variables or use user-secrets for development
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
 
+#endregion
+
+#region Mail Settings
+
 // Init MailSettings from environment variables or use user-secrets for development
 builder.Services.Configure<MailSettings>(builder.Configuration.GetSection("MailSettings"));
 
-// Add services to the container.
+#endregion
+
+#region JSON Options
+
+// Customize JSON serialization to handle enums as strings
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    { 
+        options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+    });
+
+#endregion
+
+#region Model Validation
+
+// Customize the response for model validation errors
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.InvalidModelStateResponseFactory = context =>
+    {
+        var errors = context.ModelState
+                                    .Where(e => e.Value.Errors.Count > 0)
+                                    .SelectMany(e => e.Value.Errors.Select(er => er.ErrorMessage))
+                                    .ToList();
+
+        var response = new ErrorResponseDTO
+        {
+            StatusCode = (int)HttpStatusCode.BadRequest,
+            Error = "One or more validation errors occurred.",
+            ValidationErrors = errors,
+            Path = context.HttpContext.Request.Path,
+            Timestamp = DateTime.UtcNow
+        };
+
+        return new BadRequestObjectResult(response);
+    };
+});
+
+#endregion
+
+#region Dependency Injection
+
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
 builder.Services.AddScoped<JwtProvider>();
@@ -70,7 +121,50 @@ builder.Services.AddScoped<UserValidator>();
 builder.Services.AddScoped<TaskItemRepository>();
 builder.Services.AddSingleton<MailService>();
 builder.Services.AddHostedService<SystemHealthService>();
+
+#endregion
+
+#region Telemetry
+
+// Add service for Azure Application Insights
 builder.Services.AddApplicationInsightsTelemetry();
+
+#endregion
+
+#region CORS
+
+// Configure CORS to allow requests from any origin (adjust as needed for production)
+var allowedOrigins = builder.Configuration
+    .GetSection("Cors:AllowedOrigins")
+    .Get<string[]>()
+    ?? (Environment.GetEnvironmentVariable("CORS_ALLOWED_ORIGINS")?
+        .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
+        ?? Array.Empty<string>());
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        if (allowedOrigins.Length > 0)
+        {
+            policy.WithOrigins(allowedOrigins)
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        }
+        else
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        }            
+    });
+});
+
+#endregion
+
+#region Swagger
+
+builder.Services.AddEndpointsApiExplorer();
 
 // Configure Swagger to accept JWT tokens
 builder.Services.AddSwaggerGen(options =>
@@ -102,49 +196,11 @@ builder.Services.AddSwaggerGen(options =>
     });
 });
 
-builder.Services.AddControllers().AddJsonOptions(options => 
-options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
-
-// Configure CORS to allow requests from any origin (adjust as needed for production)
-var allowedOrigins = builder.Configuration
-    .GetSection("Cors:AllowedOrigins")
-    .Get<string[]>()
-    ?? (Environment.GetEnvironmentVariable("CORS_ALLOWED_ORIGINS")?
-        .Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries)
-        ?? Array.Empty<string>());
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policy =>
-    {
-        if (allowedOrigins.Length > 0)
-        {
-            policy.WithOrigins(allowedOrigins)
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        }
-        else
-        {
-            policy.AllowAnyOrigin()
-                  .AllowAnyHeader()
-                  .AllowAnyMethod();
-        }            
-    });
-});
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.Configure<ApiBehaviorOptions>(options =>
-{
-    options.InvalidModelStateResponseFactory = context =>
-    {
-        return new BadRequestObjectResult(context.ModelState);
-    };
-});
+#endregion
 
 var app = builder.Build();
+
+#region Database Migration and Seeding
 
 using (var scope = app.Services.CreateScope())
 {
@@ -162,6 +218,10 @@ using (var scope = app.Services.CreateScope())
         logger.LogError(ex, "An error occurred while seeding the database.");
     }
 }
+
+#endregion
+
+#region Middleware Pipeline
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -181,5 +241,7 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+#endregion
 
 app.Run();
